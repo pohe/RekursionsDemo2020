96dae380fc06f08df864b90827bba3f7bfa5de00
Poul Henriksen <pohe@easj.dk>
2020-02-24 12:27:04 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RekursionsDemo2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RecursionEx r = new RecursionEx();
+
+            //r.PrintHello();
+            //r.PrintHello(5);
+            Console.WriteLine($"Add numbers up to 5 iterativt {r.AddNumbersIterativt(5)}");
+
+            Console.WriteLine($"Add numbers up to 5 rekursivt {r.AddNumbersRekursivt(5)}");
+
+            Console.WriteLine($"Fakultet rekursivt af 5  {r.Factorial(5)}");
+
+            Console.WriteLine($"Fibonacci af 7 {r.Fibonacci(7)}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RecursionEx.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RekursionsDemo2020
+{
+    public class RecursionEx
+    {
+
+        public void PrintHello()
+        {
+            Console.WriteLine("Hello");
+            PrintHello();
+        }
+
+        public void PrintHello(int antalKald)
+        {
+            if (antalKald > 0)
+            {
+                Console.WriteLine("Hello");
+                PrintHello(antalKald - 1);
+            }
+            
+        }
+
+
+        public int AddNumbersIterativt(int number)
+        {
+            int sum = 0;
+            for (int i = 0; i <= number; i++)
+            {
+                sum = sum + i;
+            }
+
+            return sum;
+        }
+
+
+        public int AddNumbersRekursivt(int number)
+        {
+            
+            if (number > 0)
+            { 
+                return  number + AddNumbersRekursivt(number - 1);
+            }
+
+            return 0;
+        }
+
+        public int Factorial(int number)
+        {
+            //if (number > 0)
+            //{
+            //    return number * Factorial(number - 1);
+            //}
+            //return 1; 
+
+            return (number > 0) ? number * Factorial(number - 1) : 1;
+        }
+
+        //public int FactorialIterativt(int number)
+        //{
+        // Implementer denne selv
+        //}
+
+        public int Fibonacci(int n)
+        {
+            if (n < 3)
+            {
+                return 1;
+            }
+            else
+            {
+                return (Fibonacci(n - 1) + Fibonacci(n - 2));
+            }
+
+            //return (n < 3) ? 1 : (Fibonacci(n - 1) + Fibonacci(n - 2));
+        }
+
+        //public int FibonacciIterativt(int number)
+        //{
+        // Implementer denne selv
+        //}
+
+    }
+}
