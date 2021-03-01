ee6bfd8818e4c7cd564db565e3768a96f9c3dda0
Poul Henriksen <pohe@easj.dk>
2021-03-01 08:15:42 +0100

    Opdateret

--- Program.cs
+++ Program.cs
@@ -27,8 +27,8 @@ namespace RekursionsDemo2020
             //r.ReverseStringIterativ("aksel");
             //r.ReverseStringRekursivt("aksel");
 
-            string omvendt = r.ReverseStringRekursivttxt("aksel");
-            Console.WriteLine(omvendt);
+            //string omvendt = r.ReverseStringRekursivttxt("aksel");
+            //Console.WriteLine(omvendt);
             //Console.WriteLine($"Fibonacci af 6 {r.Fibonacci(6)}");
             //Console.WriteLine($"Fibonacci af 6 {r.FibonacciIterativt(6)}");
 
@@ -37,12 +37,12 @@ namespace RekursionsDemo2020
             //RecursionEx.Node mor= r.DanTræ();
             //r.PrintTal(mor);
 
-            //string pegA = "pegA";
-            //string pegB = "pegB";
-            //string pegC = "pegC";
+            string pegA = "pegA";
+            string pegB = "pegB";
+            string pegC = "pegC";
 
 
-            //r.TowersOfHanoi(pegA, pegB, pegC, 4);
+            r.TowersOfHanoi(pegA, pegB, pegC, 3);
 
             Console.ReadLine();
         }
